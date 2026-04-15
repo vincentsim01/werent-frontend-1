d00d3b8463869d1d20739bb014006ad8e5da47c2
kebejoan <jmailed.joe@gmail.com>
2026-04-15 16:06:07 +0700

    feat(services): Trial fetch from json-server

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+import { fetchProduct } from "@/services";
+
+export default async function Home() {
+	const product = await fetchProduct(2);
+
 	return (
 		<div>
 			{/* BODY */}
 			<div className="w-full || h-500">Home</div>
+			<div className="text-5xl font-bold">{product.name}</div>
 		</div>
 	);
 }
