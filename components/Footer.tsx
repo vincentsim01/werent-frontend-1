885761f0f6af56ef754e0c33964039b9dc71ad97
kebejoan <jmailed.joe@gmail.com>
2026-04-15 15:17:38 +0700

    feat(footer): adjust margin top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<div className="w-full outline-2 outline-b outline-gray-500 p-2 bg-werent-green-1 text-white || text-center h-100 ">
+		<div className="w-full outline-2 outline-b outline-gray-500 p-2 bg-werent-green-1 text-white mt-10 || text-center h-100 ">
 			THIS IS A FOOTER
 		</div>
 	);
