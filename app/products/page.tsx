426085cbd203b9f8024340834099d03643abbaa8
kebejoan <jmailed.joe@gmail.com>
2026-04-15 15:52:47 +0700

    feat(products): finish products page wireframe

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,21 @@
+import ProductCard from "@/components/ProductCard";
+
 export default function ProductsPage() {
 	return (
-		<div className="flex flex-col gap-2">
-			<div className="w-full || h-500">Products Page</div>
+		<div className="mx-auto w-full xl:w-[1280px] flex flex-col justify-center gap-2">
+			<div className="|| w-full text-2xl font-bold">
+				This is a Product list Page (string to delete)
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+					{/* MAP ProductCard */}
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+				</div>
+			</div>
 			{/* ^^^ div to be deleted */}
-			<div></div>
 		</div>
 	);
 }
---
+++ components/ProductCard.tsx
@@ -0,0 +1,7 @@
+export default function ProductCard() {
+	return (
+		<div className="w-full outline-2 outline-red-400 || aspect-[3/4]">
+			Product Card
+		</div>
+	);
+}
