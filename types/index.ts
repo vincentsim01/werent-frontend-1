1193923896c1f49303541ad9888766023aa0f104
kebejoan <jmailed.joe@gmail.com>
2026-04-16 21:03:19 +0700

    feat(service): adjust services and type due to schema change

--- types/index.ts
+++ types/index.ts
@@ -1,19 +1,21 @@
 export interface Product {
-	id: number;
-	name: string;
-	price: number;
-	rating: number;
-	imageUrls: string[];
-	reviews: Review[];
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  rating: number;
+  description: string;
+  imageUrls: string[];
+  reviews: Review[];
 }
 
 export interface Review {
-	id: number;
-	title: string;
-	reviewerName: string;
-	productId: number;
-	rating: number;
-	numUpvotes: number;
-	description: string;
-	attachmentUrl: string;
+  id: number;
+  title: string;
+  reviewerName: string;
+  productId: number;
+  rating: number;
+  numUpvotes: number;
+  description: string;
+  attachmentUrl: string;
 }
