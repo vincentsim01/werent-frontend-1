16c2e84c74e670e10bdc189fe2ed95710b10827a
Vincent <vincentkiathadi@gmail.com>
2026-04-17 13:58:02 +0800

    fix: home page add link to go to all products

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import { fetchProduct } from "@/services";
+import Link from 'next/link';
 
 export default async function Home() {
 	const product = await fetchProduct(2);
 
 	return (
-		<div>
+		<div className='flex justify-center items-center'>
 			{/* BODY */}
-			<div className="w-full || h-500">Home</div>
-			<div className="text-5xl font-bold">{product.name}</div>
+			{/* <div className="w-full || h-500">Home</div> */}
+			<Link href='/products' className='bg-[var(--werent-green-1)] text-white p-10 rounded-3xl hover:opacity-60'>Click Here to See All Products</Link>
+
 		</div>
 	);
 }
