6d4262c75faf51f277ba87b761168ef708ec9f4d
ingenioust91 <ingecendrawatisalim@gmail.com>
2026-04-18 20:18:10 +0700

    fix: ReviewSection components

--- components/ReviewSection.tsx
+++ components/ReviewSection.tsx
@@ -2,9 +2,12 @@ import ReviewCard from "./ReviewCard";
 import ReviewSummary from "./ReviewSummary";
 import {fetchReviews} from "../services/index"
 import Link from "next/link";
+import { Review } from "@/types";
 
-export default async function ReviewSection(props:{productId:number}) {
-	const Review = await fetchReviews(props.productId);
+export default async function ReviewSection(props:{productId:number, productRating:number,
+	productName:string, productBrand:string}) {
+	const Review : Review[] = await fetchReviews(props.productId)
+
 	if (Review.length === 0) {
 		return (
 			<div>No reviews yet. Be the first to review this product!</div>
@@ -14,12 +17,17 @@ export default async function ReviewSection(props:{productId:number}) {
 		return current.rating>accumulator.rating ? current : accumulator;
 	})
 
-
 	return (
 		<div>
 			<div className="w-full mb-5|| p-2">
 				<div className="flex flex-col gap-2 mb-15">
-					<ReviewSummary Review={maxReview} showPicture={false}/>
+					<ReviewSummary productId={props.productId} productRating={props.productRating}
+					productName={props.productName} productBrand={props.productBrand}
+					totalReview={Review.length} backButton={false} />
+
+					<ReviewCard Review={maxReview} showPicture={true}/>
+					
+					
 					<div className='flex justify-start items-center'>
 						<Link href={`/products/${props.productId}/reviews`} className='bg-[var(--werent-green-2)] rounded-3xl pl-5 pr-5 pt-3 pb-3 text-white md:w-[20%] w-full text-center hover:opacity-60 transition-opacity duration-200'>
 							Browse All Reviews
