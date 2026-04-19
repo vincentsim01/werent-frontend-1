02a7a6cad18500ebbacefe8f42e63c223075fb9e
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2026-04-19 12:42:06 +0700

    feat: product section feature

--- components/ProductSection.tsx
+++ components/ProductSection.tsx
@@ -1,27 +1,30 @@
 import Breadcrumbs from "./Breadcrumbs";
 import Carousel from "./Carousel";
 import ProductContent from "./ProductContent";
-import {fetchProduct} from '@/services/index';
+import { fetchProduct } from "@/services/index";
 
-export default async function ProductSection({ productId }: { productId: number }) {
-	const product = await fetchProduct(productId);
+export default async function ProductSection({
+  productId,
+}: {
+  productId: number;
+}) {
+  const product = await fetchProduct(productId);
 
-	return (
-		<div>
-			<div className="w-full outline-2 outline-b outline-gray-500 || p-2">
-				This box is the product section (string to delete)
-				<div className="flex-col gap-2">
-					<Breadcrumbs />
-					<div className="flex flex-col lg:flex-row gap-2">
-						<div className="w-full lg:w-2/3">
-							<Carousel />
-						</div>
-						<div className="w-full lg:w-1/3">
-							<ProductContent />
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="w-full p-2">
+        <div className="flex flex-col gap-2">
+          <Breadcrumbs productName={product.name} />
+          <div className="flex flex-col lg:flex-row gap-2">
+            <div className="w-full lg:w-2/3">
+              <Carousel images={product.imageUrls} />
+            </div>
+            <div className="w-full lg:w-1/3">
+              <ProductContent />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
