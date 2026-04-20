57630125345f8f9c174153468175ffd607afecb4
Vincent <vincentkiathadi@gmail.com>
2026-04-21 01:06:34 +0800

    add: add review button review page

--- app/products/[id]/reviews/page.tsx
+++ app/products/[id]/reviews/page.tsx
@@ -5,7 +5,7 @@ import { fetchProduct } from "@/services/index";
 import ReviewsInfiniteList from "@/components/ReviewsInfiniteList";
 import AddToCartMobile from "@/components/AddToCart_Mobile";
 import { Review } from "@/types";
-
+import Link from "next/link";
 import { Product } from "@/types";
 
 export default async function AllReviews({
@@ -39,14 +39,20 @@ export default async function AllReviews({
 	const averageRating = allreview.reduce((acc, item) => acc + item.rating, 0) / allreview.length;
 	return (
 		<div className="mx-auto w-full xl:w-[1280px] flex flex-col justify-center gap-2 px-10 lg:px-30">
-				<div className="flex flex-col gap-2 relative">
-					<div className="sticky left-0 top-20 bg-[#f3f3f3] w-full">
+				<div className="flex flex-col gap-3 relative">
+					<div className="sticky left-0 top-10 md:top-20 bg-[#f3f3f3] w-full pb-5">
 						<div className="py-5">
 							<ReviewSummary productId={Number(id)} productRating={products.rating}
 							productName={products.name} productBrand={products.brand}
 							totalReview={reviews.length} backButton={true}/>
 						</div>
+						<div className='md:flex md:justify-end md:items-center'>
+							<Link href={`/AddReview`} className='bg-[var(--werent-green-2)] rounded-3xl pl-5 pr-5 pt-3 pb-3 text-white md:w-[20%] w-full text-center hover:opacity-60 transition-opacity duration-200'>
+								Add Review
+							</Link>
+						</div>
 					</div>
+
 					<div>
 						<section
 								id="product-reviews"
