318a2cddd9f18f60479d40ac1faff3470d18dd00
ingenioust91 <ingecendrawatisalim@gmail.com>
2026-04-20 12:03:46 +0700

    fix: add best review

--- components/ReviewSection.tsx
+++ components/ReviewSection.tsx
@@ -1,33 +1,51 @@
 import ReviewCard from "./ReviewCard";
 import ReviewSummary from "./ReviewSummary";
-import {fetchReviews} from "../services/index"
-import Link from "next/link";
+import {fetchReviews, fetchProduct} from "../services/index"
 import { Review } from "@/types";
+import Link from "next/link";
 
 export default async function ReviewSection(props:{productId:number, productRating:number,
 	productName:string, productBrand:string}) {
-	const Review : Review[] = await fetchReviews(props.productId)
-
-	if (Review.length === 0) {
+  	const ReviewRaw = await fetchReviews(props.productId);
+  	const Review = ReviewRaw.data;
+	const Product = await fetchProduct(props.productId);
+	const reviews = Product.reviews ?? [];
+	if (reviews.length === 0) {
 		return (
 			<div>No reviews yet. Be the first to review this product!</div>
 		);
 	}
-	const maxReview = Review.reduce((accumulator,current) => {
-		return current.rating>accumulator.rating ? current : accumulator;
-	})
+
+type MaxReview = {
+  value: number;
+  index: number;
+  review: Review;
+};
+
+const maxReview = reviews.reduce<MaxReview>(
+  (max, review, index) => {
+    if (review.numUpvotes > max.value) {
+      return { value: review.numUpvotes, index, review };
+    }
+    return max;
+  },
+  { value: -Infinity, index: 0, review: reviews[0] }
+);
+
+	const bestReview : Review | null = Review.length
+								? Review.reduce((prev, current)=> current.numUpvotes > prev.numUpvotes ? current : prev)
+								: null
+
 
 	return (
 		<div>
 			<div className="w-full mb-5|| p-2">
 				<div className="flex flex-col gap-2 mb-15">
 					<ReviewSummary productId={props.productId} productRating={props.productRating}
-					productName={props.productName} productBrand={props.productBrand}
-					totalReview={Review.length} backButton={false} />
+					productName={props.productName} productBrand={props.productBrand} Review={maxReview.review}
+					totalReview={reviews.length} backButton={false} />
 
-					<ReviewCard Review={maxReview} showPicture={true}/>
-					
-					
+					{bestReview && <ReviewCard Review ={bestReview} showPicture={true}/>}
 					<div className='flex justify-start items-center'>
 						<Link href={`/products/${props.productId}/reviews`} className='bg-[var(--werent-green-2)] rounded-3xl pl-5 pr-5 pt-3 pb-3 text-white md:w-[20%] w-full text-center hover:opacity-60 transition-opacity duration-200'>
 							Browse All Reviews
